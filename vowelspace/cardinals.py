702270fa559fd9c3e6627ed42ccb333635f2036a
brunoczim <brunoczim@gmail.com>
2020-12-17 21:29:38 -0300

    documentation

--- vowelspace/cardinals.py
+++ vowelspace/cardinals.py
@@ -3,6 +3,9 @@ import numpy as np
 from scipy.interpolate import Rbf 
 
 class Cardinal(NamedTuple):
+    '''
+    Data of a cardinal vowel.
+    '''
     f1: float
     f2: float
     roundedness: float
@@ -10,26 +13,46 @@ class Cardinal(NamedTuple):
     backness: float
 
 def to_f1_array(cards: List[Cardinal]) -> np.ndarray:
+    '''
+    Converts a list of cardinals to an array of F1 frequencies.
+    '''
     f1s = list(map(lambda card: card.f1, cards))
     return np.array(f1s)
 
 def to_f2_array(cards: List[Cardinal]) -> np.ndarray:
+    '''
+    Converts a list of cardinals to an array of F2 frequencies.
+    '''
     f2s = list(map(lambda card: card.f2, cards))
     return np.array(f2s)
 
 def to_roundedness_array(cards: List[Cardinal]) -> np.ndarray:
+    '''
+    Converts a list of cardinals to an array of roundness levels.
+    '''
     roundednesss = list(map(lambda card: card.roundedness, cards))
     return np.array(roundednesss)
 
 def to_height_array(cards: List[Cardinal]) -> np.ndarray:
+    '''
+    Converts a list of cardinals to an array of heights.
+    '''
     heights = list(map(lambda card: card.height, cards))
     return np.array(heights)
 
 def to_backness_array(cards: List[Cardinal]) -> np.ndarray:
+    '''
+    Converts a list of cardinals to an array of backnesses.
+    '''
     backnesss = list(map(lambda card: card.backness, cards))
     return np.array(backnesss)
 
 def to_height_arrays(cards: List[Cardinal]) -> List[np.ndarray]:
+    '''
+    Converts a list of cardinals to a list of arrays, where each array is an
+    array of a cardinal vowels' field, such that the "output" array is the
+    array of heights.
+    '''
     return [
             to_f1_array(cards),
             to_f2_array(cards),
@@ -38,6 +61,11 @@ def to_height_arrays(cards: List[Cardinal]) -> List[np.ndarray]:
             ]
 
 def to_backness_arrays(cards: List[Cardinal]) -> List[np.ndarray]:
+    '''
+    Converts a list of cardinals to a list of arrays, where each array is an
+    array of a cardinal vowels' field, such that the "output" array is the
+    array of heights.
+    '''
     return [
             to_f1_array(cards),
             to_f2_array(cards),
@@ -46,14 +74,26 @@ def to_backness_arrays(cards: List[Cardinal]) -> List[np.ndarray]:
             ]
 
 def make_rbf(arrays: List[np.ndarray]) -> Rbf:
+    '''
+    Converts a list of arrays of the same size into a function that
+    interpolates the points formed by corresponding elements of the array.
+    '''
     return Rbf(*arrays, function='cubic')
 
 def make_height_fn() -> Rbf:
+    '''
+    Interpolates a height function from cardinals.
+    '''
     return make_rbf(to_height_arrays(cardinals))
 
 def make_backness_fn() -> Rbf:
+    '''
+    Interpolates a backness function from cardinals.
+    '''
     return make_rbf(to_backness_arrays(cardinals))
 
+# Data for interpolation. Not only helps interpolation but also delimites
+# minimum and maximum.
 cardinals = [
         Cardinal(f1=240, f2=2400, roundedness=0, height=3, backness=0),
         Cardinal(f1=235, f2=2100, roundedness=1, height=3, backness=0),
--- vowelspace/fmt.py
+++ vowelspace/fmt.py
@@ -1,2 +1,5 @@
 def show_float(num: float) -> str:
+    '''
+    Formats a float number up to 6 decimal places, cutting trailing zeroes
+    '''
     return '{0:.6f}'.format(num).rstrip('0').rstrip('.')
